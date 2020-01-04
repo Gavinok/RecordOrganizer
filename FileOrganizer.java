f41dcd73b6ec8939ef8e220e4889e533aa2c25de
Gavin Jaeger-Freeborn <gavinfreeborn@gmail.com>
2020-01-04 16:49:54 -0700

    reformated FileOrganizer

--- FileOrganizer.java
+++ FileOrganizer.java
@@ -17,142 +17,143 @@ import java.io.*;
 import java.util.*;
 public class FileOrganizer 
 {
-    /*
-    * the constructor is used to take the file check if it exists and if it does it sorts the records in descending order
-    * with the SortDescending method.
-    */
-    public FileOrganizer(String filename)  throws FileNotFoundException
-    {
-            File inputFile;
-            inputFile = new File(filename);
-            if (!inputFile.exists()) 
-            { 
-                System.out.println(inputFile.getName() + " does not exist"); 
-                return;
-            }
-            SortDescending(inputFile);
+	/*
+	 * the constructor is used to take the file check if it exists and if it does it sorts the records in descending order
+	 * with the SortDescending method.
+	 */
+	public FileOrganizer(String filename)  throws FileNotFoundException
+	{
+		File inputFile;
+		inputFile = new File(filename);
+		if (!inputFile.exists()) 
+		{ 
+			System.out.println(inputFile.getName() + " does not exist"); 
+			return;
+		}
+		SortDescending(inputFile);
 
-    }
-    /* 
-    * SortDescending basically takes the file used in the constructor and sorts the student names and numbers in descending order.
-    */
-    private void SortDescending(File inputFile) throws FileNotFoundException
-    {
-        RandomAccessFile raf = null;
-       try 
-       { 
-            raf = new RandomAccessFile(inputFile, "rw"); 
-            String line = "1";
-            String lastline = "1";
-            int StudentNumber0 = 0;
-            int StudentNumber1 = 0;
-            long locationOfLastLine = raf.getFilePointer();
-            long locationOfCurrentLine = raf.getFilePointer();
-            locationOfLastLine = raf.getFilePointer();
-            locationOfLastLine = raf.getFilePointer();
-            lastline = raf.readLine();
-            StudentNumber0 = parseLine(lastline);
-            boolean switched = true;
-            boolean skip = false;
+	}
 
-            //if a switch has occurred start from the top until no switch if preformed
-           while(switched == true)
-            { 
-                switched = false;
-                //while we have not reached the end of the file
-                while(raf.getFilePointer() < raf.length())
-                {
-                        
-                    locationOfCurrentLine = raf.getFilePointer();
-                    line = raf.readLine();
-                    StudentNumber1 = parseLine(line);
-                    // if a switch is needed and we did not just restart the while loop
-                    if((StudentNumber1 > StudentNumber0) && (skip == false))
-                    {
-                        switched = true;
-                        String templine = lastline;
-                        raf.seek(locationOfLastLine);
-                        raf.write(PrepLine(line));
-                        raf.seek(locationOfLastLine);
-                        lastline = raf.readLine();
-                        raf.seek(locationOfCurrentLine);
-                        raf.write(PrepLine(templine));
-                        raf.seek(locationOfCurrentLine);
-                        line = raf.readLine();
-                    }// if no switch was needed continue
-                    else
-                    {
-                        StudentNumber0 = StudentNumber1;
-                        lastline = line;
-                        locationOfLastLine = locationOfCurrentLine;
-                    }
-                    skip = false;
-                }
-                // to prevent an return to the start of the record if a switch was made
-                if(switched == true)
-                {
-                    raf.seek(0);
-                    skip = true;
-                }
-            }
-           
-        }
-       catch(IOException e) 
-       {
-           System.out.println("nope SortAccending");
-       } 
-       finally
-       {
-           // if raf contains a file
-           if(!(raf == null))
-           {
-               try
-               {
-                   raf.close(); // close file
-               }
-                catch(IOException ioe) {}
-           }
-           
-       }
+	/* 
+	 * SortDescending basically takes the file used in the constructor and sorts the student names and numbers in descending order.
+	 */
+	private void SortDescending(File inputFile) throws FileNotFoundException
+	{
+		RandomAccessFile raf = null;
+		try 
+		{ 
+			raf = new RandomAccessFile(inputFile, "rw"); 
+			String line = "1";
+			String lastline = "1";
+			int StudentNumber0 = 0;
+			int StudentNumber1 = 0;
+			long locationOfLastLine = raf.getFilePointer();
+			long locationOfCurrentLine = raf.getFilePointer();
+			locationOfLastLine = raf.getFilePointer();
+			locationOfLastLine = raf.getFilePointer();
+			lastline = raf.readLine();
+			StudentNumber0 = parseLine(lastline);
+			boolean switched = true;
+			boolean skip = false;
 
-        
-    }
-    // PrepLine simply takes a string and makes it into an array of 36 bytes
-    private byte[] PrepLine(String line)
-    {
-        //since a string consists of characters containing 2 bytes each
-        while(line.getBytes().length  < 36)
-        {
-            line += " ";
-        }
-        return line.getBytes();
-    }
-    // parseLine is used to extract a number from a sting 
-    static private int parseLine(String line)
-    {
-        Scanner scan = new Scanner(line);
-        return scan.nextInt();
-    }
+			//if a switch has occurred start from the top until no switch if preformed
+			while(switched == true)
+			{ 
+				switched = false;
+				//while we have not reached the end of the file
+				while(raf.getFilePointer() < raf.length())
+				{
 
-    // main is used to create the FileOrganizer class with the appropriate student_record.txt file
-    public static void main (String[] args) 
-    {
-        try 
-        { 
-            FileOrganizer studentfile = new FileOrganizer("student_record.txt");
-        }
-        catch(IOException e) 
-        {
-            System.out.println("nope main");
-        }
-        try 
-        { 
-            FileOrganizer studentfile = new FileOrganizer("student_record.txt");
-        }
-        catch(IOException e) 
-        {
-            System.out.println("nope main");
-        }
+					locationOfCurrentLine = raf.getFilePointer();
+					line = raf.readLine();
+					StudentNumber1 = parseLine(line);
+					// if a switch is needed and we did not just restart the while loop
+					if((StudentNumber1 > StudentNumber0) && (skip == false))
+					{
+						switched = true;
+						String templine = lastline;
+						raf.seek(locationOfLastLine);
+						raf.write(PrepLine(line));
+						raf.seek(locationOfLastLine);
+						lastline = raf.readLine();
+						raf.seek(locationOfCurrentLine);
+						raf.write(PrepLine(templine));
+						raf.seek(locationOfCurrentLine);
+						line = raf.readLine();
+					}// if no switch was needed continue
+					else
+					{
+						StudentNumber0 = StudentNumber1;
+						lastline = line;
+						locationOfLastLine = locationOfCurrentLine;
+					}
+					skip = false;
+				}
+				// to prevent an return to the start of the record if a switch was made
+				if(switched == true)
+				{
+					raf.seek(0);
+					skip = true;
+				}
+			}
 
-    }
+		}
+		catch(IOException e) 
+		{
+			System.out.println("nope SortAccending");
+		} 
+		finally
+		{
+			// if raf contains a file
+			if(!(raf == null))
+			{
+				try
+				{
+					raf.close(); // close file
+				}
+				catch(IOException ioe) {}
+			}
+
+		}
+
+
+	}
+	// PrepLine simply takes a string and makes it into an array of 36 bytes
+	private byte[] PrepLine(String line)
+	{
+		//since a string consists of characters containing 2 bytes each
+		while(line.getBytes().length  < 36)
+		{
+			line += " ";
+		}
+		return line.getBytes();
+	}
+	// parseLine is used to extract a number from a sting 
+	static private int parseLine(String line)
+	{
+		Scanner scan = new Scanner(line);
+		return scan.nextInt();
+	}
+
+	// main is used to create the FileOrganizer class with the appropriate student_record.txt file
+	public static void main (String[] args) 
+	{
+		try 
+		{ 
+			FileOrganizer studentfile = new FileOrganizer("student_record.txt");
+		}
+		catch(IOException e) 
+		{
+			System.out.println("nope main");
+		}
+		try 
+		{ 
+			FileOrganizer studentfile = new FileOrganizer("student_record.txt");
+		}
+		catch(IOException e) 
+		{
+			System.out.println("nope main");
+		}
+
+	}
 }
